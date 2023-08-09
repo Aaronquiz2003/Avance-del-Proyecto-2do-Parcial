99a7af066346368a6dc95031f1a9fe008548f901
Aaronquiz2003 <135557233+Aaronquiz2003@users.noreply.github.com>
2023-08-09 12:18:53 -0500

    Update persona_principal.py

--- servicio/persona_principal.py
+++ servicio/persona_principal.py
@@ -1,6 +1,7 @@
+
 from PySide6 import QtGui
 from PySide6.QtGui import QRegularExpressionValidator
-from PySide6.QtWidgets import QMainWindow, QMessageBox
+from PySide6.QtWidgets import QMainWindow, QMessageBox, QApplication
 from Datos.estudiante_dao import EstudianteDao
 from UI.vtn_principal import Ui_vtn_principal
 from dominio.docente import Docente
@@ -14,10 +15,13 @@ class PersonaPrincipal(QMainWindow):
         self.ui.stb_estado.showMessage('Bienvenido', 2000)
         self.ui.vtn_grabar.clicked.connect(self.grabar)
         self.ui.txt_cedula.setValidator(QtGui.QIntValidator())
+        self.ui.btn_buscar_cedula.clicked.connect(self.buscar_x_cedula)
+        #self.ui.btn_estatura.clicked.connect(self.calculos_estatura)
 
         correo_exp = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         validator = QRegularExpressionValidator(correo_exp, self)
         self.ui.txt_email.setValidator(validator)
+
 
     def grabar(self):
         tipo_persona = self.ui.cb_tipo_persona.currentText()
@@ -28,6 +32,14 @@ class PersonaPrincipal(QMainWindow):
             persona = None
             if tipo_persona == 'Docente':
                 persona = Docente()
+                persona.nombre = self.ui.txt_nombre.text()
+                persona.apellido = self.ui.txt_apellido.text()
+                persona.cedula = self.ui.txt_cedula.text()
+                persona.email = self.ui.txt_email.text()
+                persona.carrera = self.ui.txt_carrera.text()
+                respuesta = None
+                respuesta = EstudianteDao.insertar_estudiante(persona)
+                persona.estatura = self.ui.sp_estatura.text()
             else:
                 persona = Estudiante()
 
@@ -36,9 +48,10 @@ class PersonaPrincipal(QMainWindow):
             persona.cedula = self.ui.txt_cedula.text()
             persona.email = self.ui.txt_email.text()
             persona.carrera = self.ui.txt_carrera.text()
+            #persona.estatura= self.ui.txt
             respuesta=None
             respuesta= EstudianteDao.insertar_estudiante(persona)
-
+           # persona.estatura = self.ui.sp_estatura.text()
             try:
                 EstudianteDao.insertar_estudiante(persona)  # Asumiendo que EstudianteDao.insertar_estudiante está definido en tu código.
                 self.ui.txt_nombre.setText('')
@@ -46,24 +59,20 @@ class PersonaPrincipal(QMainWindow):
                 self.ui.txt_cedula.setText('')
                 self.ui.txt_email.setText('')
                 self.ui.txt_carrera.setText('')
+                # persona.estatura = self.ui.sp_estatura.text()
                 self.ui.stb_estado.showMessage('Grabado con éxito.', 2000)
             except Exception as e:
                 QMessageBox.critical(self, 'Error', f'No se pudo grabar: {str(e)}')
 
     def buscar_x_cedula(self):
-        cedula = self.ui.txt_consulta_cedula.text()
-        e= Estudiante(cedula=cedula)
-        e=EstudianteDao.seleccionar_por_cedula(e)
+        cedula = self.ui.txt_cedula.text()
+        e = Estudiante(cedula=cedula)
+        e = EstudianteDao.seleccionar_por_cedula(e)
         print(e)
         self.ui.txt_nombre.setText(e.nombre)
         self.ui.txt_apellido.setText(e.apellido)
         self.ui.txt_email.setText(e.email)
+        self.ui.txt_carrera.setText(e.carrera)
         self.ui.cb_tipo_persona.setCurrentText('Estudiante')
 
-if __name__ == "__main__":
-    import sys
 
-    app = QApplication(sys.argv)
-    ventana = PersonaPrincipal()
-    ventana.show()
-    sys.exit(app.exec())
